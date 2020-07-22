e39c6b657f1457e861927232795cff4747bdee2d
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-07-22 17:25:22 +0100

    feat: add an example to test crashlytics

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,59 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useState } from 'react';
+import crashlytics from '@react-native-firebase/crashlytics';
+import { View, Button, Text } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
-import { AppConfigProvider, ThemeProvider, LocaleProvider, SplashScreen } from '../shared/modules';
+import {
+    AppConfigProvider,
+    ThemeProvider,
+    LocaleProvider,
+    SplashScreen,
+} from '../shared/modules';
 import Navigation, { rootNavigation } from '../navigation';
 import localeConfig from '../../intl';
 
-const App = () => (
-    <AppConfigProvider>
-        <SafeAreaProvider>
-            <ThemeProvider>
-                <LocaleProvider { ...localeConfig }>
-                    <SplashScreen>
-                        <NavigationContainer ref={ rootNavigation.navigationRef }>
-                            <Navigation />
-                        </NavigationContainer>
-                    </SplashScreen>
-                </LocaleProvider>
-            </ThemeProvider>
-        </SafeAreaProvider>
-    </AppConfigProvider>
-);
+const App = () => {
+    const [enabled, setEnabled] = useState(
+        crashlytics().isCrashlyticsCollectionEnabled,
+    );
+
+    async function toggleCrashlytics() {
+        await crashlytics()
+            .setCrashlyticsCollectionEnabled(!enabled)
+            .then(() =>
+                setEnabled(crashlytics().isCrashlyticsCollectionEnabled),
+            );
+    }
+
+    return (
+        <AppConfigProvider>
+            <SafeAreaProvider>
+                <ThemeProvider>
+                    <LocaleProvider { ...localeConfig }>
+                        <SplashScreen>
+                            <NavigationContainer
+                                ref={ rootNavigation.navigationRef }>
+                                <Navigation />
+                                <View>
+                                    <Button
+                                        title="Toggle Crashlytics"
+                                        onPress={ toggleCrashlytics } />
+                                    <Button
+                                        title="Crash"
+                                        onPress={ () => crashlytics().crash() } />
+                                    <Text>
+                                        Crashlytics is currently{' '}
+                                        {enabled ? 'enabled' : 'disabled'}
+                                    </Text>
+                                </View>
+                            </NavigationContainer>
+                        </SplashScreen>
+                    </LocaleProvider>
+                </ThemeProvider>
+            </SafeAreaProvider>
+        </AppConfigProvider>
+    );
+};
 
 export default App;
