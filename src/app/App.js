e289a17fad9f537faf7e60ffd9cde2d001a1b3c8
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-07-23 17:00:15 +0100

    feat: add rollbar

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Client } from 'rollbar-react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import appConfig from 'react-native-config';
 import { AppConfigProvider, ThemeProvider, LocaleProvider, SplashScreen } from '../shared/modules';
 import Navigation, { rootNavigation } from '../navigation';
 import localeConfig from '../../intl';
+
+const rollbar = new Client(appConfig.POST_CLIENT_ITEM_ACCESS_TOKEN);
+
+rollbar.log('Rollbar log');
 
 const App = () => (
     <AppConfigProvider>
