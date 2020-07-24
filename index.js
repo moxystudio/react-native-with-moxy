33422bdaf5cd965abae0fd43c0bc36af39e43fc2
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-07-24 15:19:22 +0100

    feat: add bugsnag

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import './shims';
 
 import { AppRegistry } from 'react-native';
+import { Client } from 'bugsnag-react-native';
+import appConfig from 'react-native-config';
 import App from './src/app/App';
 import { name as appName } from './app.json';
+
+const bugsnag = new Client(appConfig.BUGSNAG_TOKEN);
+
+bugsnag.notify(new Error('Test error'));
 
 AppRegistry.registerComponent(appName, () => App);
